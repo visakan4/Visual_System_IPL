6340448fc8d1b917dfdbfe1a2b04bb92261ab85c
Siddhant Bhardwaj <siddhant.bhardwaj16@gmail.com>
2017-10-16 15:44:55 -0300

    initial commit : frontend app setup

---
+++ src/apps/Dashboard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import * as d3 from 'd3';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>This is our dashboard</div>
+    );
+  }
+  
+  componentDidMount() {
+    // Write your d3js code here
+  }
+}
+
+export default Dashboard;
