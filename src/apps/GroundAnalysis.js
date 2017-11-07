a2114e00110bcac75be7c812e7d1e4427f37ffab
visakan <visakan4@gmail.com>
2017-11-07 16:55:55 -0400

    First Commmit!!Yayy

--- src/apps/GroundAnalysis.js
+++ src/apps/GroundAnalysis.js
@@ -1,17 +1,144 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
 import '../styles/groundAnalysis.css';
+import $ from 'jquery';
+import data from './data.js';
 
 class GroundAnalysis extends Component {
   render() {
     return (
-      <div id="canvas" className="groundCanvas">Ground Analysis Component</div>
+	 <div id="container">
+	  <p>Ground Analysis</p>
+      <label for="groundNames">Ground Name</label>
+	  <select id="groundNames" dir="rt1" onChange={this.drawCharts.bind(this)} onload="drawCharts(this)"></select>
+	  <div id="container1" className="grid">
+
+	  </div>
+	  <div id="container2" className="grid">
+
+	  </div>
+	</div>
     );
   }
   
   componentDidMount() {
-    // Write your d3js code here
+  	this.initializeVars();
+  	// var value = this.init();
+  	this.appendValues();
+  	this.createSVGElement();
   }
+
+  initializeVars() {
+  	this.svg = null;
+  	this.width = null;
+  	this.height = null;
+  	this.radius = null;
+  	this.svg1 = null;
+  	this.arc =  null;
+  	this.textArc = null;
+  	this.pie = null;
+  	this.actual_JSON = data;
+  	this.selectValues = null;
+  }
+
+
+  appendValues() {
+    // this.actual_JSON = JSON.parse(this.actual_JSON); 
+    $.each(this.actual_JSON, (index, value) => {
+        $('select').append($('<option>').text(this.actual_JSON[index].groundName).val(this.actual_JSON[index].groundID));
+    });
+  }
+
+  createSVGElement(){
+    this.width = 400;
+    this.height = 300;
+    this.radius = Math.min(this.width,this.height)/2;
+
+    this.svg = d3.select('#container1')
+                .append("svg")
+                .attr("width",this.width)
+                .attr("height",this.height)
+                .append("g")
+                .attr("transform","translate(" + this.width/2 + "," + this.height/2 +")")
+                .attr("class","pieCharts");
+
+
+    this.svg1 = d3.select('#container2')
+                .append("svg")
+                .attr("width",this.width)
+                .attr("height",this.height)
+                .append("g")
+                .attr("transform","translate(" + this.width/2 + "," + this.height/2 +")")
+                .attr("class","pieCharts");
+
+    this.arc = d3.arc()
+            .outerRadius(this.radius - 10)
+            .innerRadius(0);
+
+    this.textArc = d3.arc()
+                .outerRadius(this.radius - 30)
+                .innerRadius(this.radius - 20);
+
+
+    this.pie = d3.pie()
+                .value(function(d) {
+                    return d;
+                });
+   }
+
+drawCharts(event){
+    var selectedOption = event.target.options[event.target.selectedIndex];
+    var index = selectedOption.value;
+    var tossarray = [];
+    var battingArray = [];
+    tossarray.push(this.actual_JSON[index].winPercentageWinningToss,this.actual_JSON[index].winPercentageLosingToss);
+    battingArray.push(this.actual_JSON[index].winPercentagePlayingFirst,this.actual_JSON[index].winPercentagePlayingSecond);
+    this.drawTossWinChart(tossarray);
+    this.drawBattingFirstChart(battingArray);
+}
+
+drawTossWinChart(data){
+
+    var color = ["red","blue"];
+
+    var arcs = this.svg.selectAll("arc")
+                  .data(this.pie(data))
+                  .enter()
+                  .append("g")
+                  .attr("class","arc");
+
+    arcs.append("path").attr("d",this.arc).attr("fill",function(d,i){
+        return color[i];
+    });
+
+    arcs.append("text")
+        .attr("transform",(d)=>{
+        return "translate("+ this.textArc.centroid(d)+")"})
+        .text(function(d){
+        return d.data;
+    })
+}
+
+drawBattingFirstChart(data){
+
+    var color = ["yellow","green"];
+
+    var arcs = this.svg1.selectAll("arc")
+                  .data(this.pie(data))
+                  .enter()
+                  .append("g")
+                  .attr("class","arc");
+
+    arcs.append("path").attr("d",this.arc).attr("fill",function(d,i){
+        return color[i];
+    });
+
+    arcs.append("text").attr("transform",(d)=>{
+        return "translate(" + this.textArc.centroid(d) + ")"
+    }).text(function(d){
+        return d.data;
+    });
+}
 }
 
 export default GroundAnalysis;
---
+++ src/apps/data.js
@@ -0,0 +1,103 @@
+var data = [		
+	{	
+		"groundID":0,
+		"groundName":"M Chinnaswamy Stadium",
+		"numberofMatchesPlayed":50,
+		"winPercentagePlayingFirst":80,
+		"winPercentagePlayingSecond":20,
+		"winPercentageWinningToss":60,
+		"winPercentageLosingToss":40
+	},
+	{	
+		"groundID":1,
+		"groundName":"Feroz Shah Kotla",
+		"numberofMatchesPlayed":10,
+		"winPercentagePlayingFirst":70,
+		"winPercentagePlayingSecond":30,
+		"winPercentageWinningToss":50,
+		"winPercentageLosingToss":50
+	},
+	{	
+		"groundID":2,
+		"groundName":"Wankhede Stadium",
+		"numberofMatchesPlayed":60,
+		"winPercentagePlayingFirst":25,
+		"winPercentagePlayingSecond":75,
+		"winPercentageWinningToss":40,
+		"winPercentageLosingToss":60
+	},
+	{	
+		"groundID":3,
+		"groundName":"Eden Gardens",
+		"numberofMatchesPlayed":80,
+		"winPercentagePlayingFirst":55,
+		"winPercentagePlayingSecond":45,
+		"winPercentageWinningToss":25,
+		"winPercentageLosingToss":75
+	},
+	{
+		"groundID":4,
+		"groundName":"MA Chidambaram Stadium, Chepauk",
+		"numberofMatchesPlayed":90,
+		"winPercentagePlayingFirst":100,
+		"winPercentagePlayingSecond":0,
+		"winPercentageWinningToss":0,
+		"winPercentageLosingToss":100
+	},
+	{
+		"groundID":5,
+		"groundName":"Dr DY Patil Sports Academy",
+		"numberofMatchesPlayed":60,
+		"winPercentagePlayingFirst":10,
+		"winPercentagePlayingSecond":90,
+		"winPercentageWinningToss":12,
+		"winPercentageLosingToss":88
+	},
+	{
+		"groundID":6,
+		"groundName":"Kingsmead",
+		"numberofMatchesPlayed":23,
+		"winPercentagePlayingFirst":64,
+		"winPercentagePlayingSecond":26,
+		"winPercentageWinningToss":100,
+		"winPercentageLosingToss":0
+	},
+	{
+		"groundID":7,
+		"groundName":"Newlands",
+		"numberofMatchesPlayed":1,
+		"winPercentagePlayingFirst":53,
+		"winPercentagePlayingSecond":47,
+		"winPercentageWinningToss":96,
+		"winPercentageLosingToss":4
+	},
+	{
+		"groundID":8,
+		"groundName":"St George's Park",
+		"numberofMatchesPlayed":124,
+		"winPercentagePlayingFirst":29,
+		"winPercentagePlayingSecond":71,
+		"winPercentageWinningToss":20,
+		"winPercentageLosingToss":80
+	},
+	{
+		"groundID":9,
+		"groundName":"SuperSport Park",
+		"numberofMatchesPlayed":54,
+		"winPercentagePlayingFirst":42,
+		"winPercentagePlayingSecond":58,
+		"winPercentageWinningToss":98,
+		"winPercentageLosingToss":2
+	},
+	{
+		"groundID":10,
+		"groundName":"Buffalo Park",
+		"numberofMatchesPlayed":57,
+		"winPercentagePlayingFirst":56,
+		"winPercentagePlayingSecond":44,
+		"winPercentageWinningToss":92,
+		"winPercentageLosingToss":8
+	}
+]
+
+export default data;
