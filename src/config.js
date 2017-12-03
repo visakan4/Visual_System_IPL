916078f500d85ad6221d44810a17f650a99374c7
Siddhant Bhardwaj <siddhant.bhardwaj16@gmail.com>
2017-12-03 14:38:35 -0400

    changed config for endpoint url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const Config = {
-  // apiEndpoint: 'https://vat-backend.herokuapp.com',
-  apiEndpoint: 'http://localhost:5000',
+  apiEndpoint: 'https://vat-backend.herokuapp.com',
+  // apiEndpoint: 'http://localhost:5000',
 }
 
 export default Config;
