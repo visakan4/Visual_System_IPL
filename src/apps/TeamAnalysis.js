4a4d46c7833aa30598b0e1b4b51fcdac09fdfa3e
visakan <visakan4@gmail.com>
2017-12-18 20:40:00 -0400

    tree map integration

--- src/apps/TeamAnalysis.js
+++ src/apps/TeamAnalysis.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 
+const divStyle = {
+  border:'none',
+  'margin-left':'180px'
+}
+
 class TeamAnalysis extends Component {
   render() {
     return (
       <div>
         <h3>Team Analysis</h3>
         <hr/>
+        {/* We have deployed these two visualisation in another server as we were facing problems with reach framework in our server*/}
         <div className="row">
           <div className="col-12">
-            <iframe title="Team analysis" src="https://visualpearl.azurewebsites.net/others/MapPage.html" width={window.outerWidth - 30} height= "600" style={{border: 'none'}}></iframe>
+            <iframe title="Team analysis" src="https://visualpearl.azurewebsites.net/others/MapPage.html" width={window.outerWidth - 30} height= "800" style={{border: 'none'}}></iframe>
           </div>
-            
           <div className="col-12">
-            <div id="treeWrapper"></div>
+            <div id="treeWrapper" style={{'text-align': 'center'}}>
+              <iframe title="Team analysis" src="https://visualpearl.azurewebsites.net/VA/treeMap.html" width={window.outerWidth - 30} height= "800" style={divStyle}></iframe>
+            </div>
           </div>
         </div>
       </div>
